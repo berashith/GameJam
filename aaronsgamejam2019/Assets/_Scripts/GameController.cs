7e744a3cc2ab95e1086a7b21ee1574764a518940
Daniel Lindenberger <daniel@daniellindenberger.com>
2019-05-11 17:06:21 -0700

    characterActions are starting to work. Character and House assets exist.

---
+++ aaronsgamejam2019/Assets/_Scripts/GameController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject NPCObj;
+
+    public List<AudioClip> characterSounds = new List<AudioClip>();
+
+    // Use this for initialization
+    void Start () {
+        // Create Characters
+        var newNPC = Instantiate(NPCObj, new Vector3(14.5f, 1.2f, -5.9f), Quaternion.identity);
+        newNPC.GetComponent<NPCScript>().characterName = "EarthHealer";
+        newNPC.GetComponent<NPCScript>().characterRole = "EarthHealer";
+        newNPC.GetComponent<NPCScript>().movementSpeed = 1; //NOT CURRENTLY IMPLEMENTED
+
+        var newAction = new characterAction();
+        newAction.isTriggered = false;
+        newAction.isReady = true;
+        newAction.isClicked = false;
+        newAction.triggerType = 1;
+
+        newAction.actionSound = characterSounds[1];
+
+        newNPC.GetComponent<NPCScript>().characterActions.Add(newAction);
+        
+        // Name
+        // Role
+        // Location
+        // triggered actions
+
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ aaronsgamejam2019/Assets/_Scripts/NPCScript.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterAction
+{
+    public bool isTriggered = false;
+    public bool isReady = false;
+    public bool isClicked = false;
+    public AudioClip actionSound;
+    public Vector3 navPoint;
+
+    /*
+     *  0 = Instant (as soon as "isReady" is true, trigger next Update()
+     *  1 = Click
+     *  2 = Proximity
+     */
+
+    public int triggerType;
+}
+
+public class NPCScript : MonoBehaviour {
+
+    public string characterName;
+    public string characterRole;
+    public float movementSpeed;
+    public AudioSource NPCVoice;
+
+
+    public bool clicked;
+
+    public List<characterAction> characterActions = new List<characterAction>();
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        checkTriggers();
+	}
+
+    public void clickNPC()
+    {
+        if (clicked == false)
+        {
+            clicked = true;
+            Debug.Log("CLICKED");
+            checkTriggers();
+        }
+    }
+
+    // Run through all triggers in list. If one is ready but not triggered, and the trigger condition is met, trigger it.
+    void checkTriggers()
+    {
+        foreach (var actionItem in characterActions)
+        {
+            if (actionItem.isReady && !actionItem.isTriggered)
+            {
+                if (actionItem.triggerType == 1 && clicked == true)
+                {
+                    actionItem.isTriggered = true;
+                    Debug.Log("TRIGGERED");
+
+                    // Is it an audio trigger? If so, trigger it
+                    NPCVoice.clip = actionItem.actionSound;
+                    NPCVoice.Play(0);
+
+                }
+            }
+        }
+    }
+}
